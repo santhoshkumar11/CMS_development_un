52839c53f88ef352f71468d9057856b9f1cdaff9
santhoshkumar11 <40330604+santhoshkumar11@users.noreply.github.com>
2020-12-04 17:07:27 +0800

    i deleted two lines of programm

--- index.php
+++ index.php
@@ -116,11 +116,7 @@
                 
                 
                 
-                <hr>
-                <p><?php echo $post_content ?></p>
-                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr>
+               
                 
 
    <?php }  } ?>
